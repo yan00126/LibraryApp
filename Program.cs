9ecfd5f9c67e29a4658ddca5417540374c840828
FeiYan <yan00126@algonquinlive.com>
2024-07-23 15:42:50 -0400

    try,exception, library app

--- Program.cs
+++ Program.cs
@@ -1,8 +1,43 @@
-﻿internal class Program
+﻿using LibraryApp;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+      var library = new Library(Helper.GetAvailableBooks());
+      while (true)
+      {
+        Console.WriteLine("Library Menu: \n1. View All Books. \n2. Add a Book: \n3. Check Out a Book. \n4. Return a Book. \n5. Exit");
+
+        string choice = GetResponse("Enter your choice: ");
+
+        switch (choice)
+        {
+          case "1":
+            // view books
+            ViewBooks(library);
+            break;
+          case "2":
+            // add a book
+            AddBook(library);
+            break;
+          case "3":
+            // checkout book
+            CheckOutBook(library);
+            break;
+          case "4":
+            //return book
+            ReturnBook(library);
+            break;
+          case "5":
+            Console.WriteLine("Thanks, Goodbye.");
+            return;
+            
+          default:
+            Console.WriteLine("Invalid choice. Please try again.");
+            break;
+        }
+      }
     }
 
     static string GetResponse (string request) 
@@ -16,5 +51,93 @@
       }
 
       return response;
+    }
+
+    static void ViewBooks (Library library)
+    {
+      library.ListBooks();
+    }
+
+    static void AddBook (Library library)
+    {
+      string title = GetResponse("Enter title: ");
+      string author = GetResponse("Enter Author: ");
+
+      bool isValid = false;
+      int year = 0;
+
+      while (!isValid)
+      {
+        isValid = int.TryParse(GetResponse("Enter year: "), out year);
+
+      }
+
+      var book = new Book(title, author, year);
+
+      try
+      {
+        library.AddBook(book);
+        Console.WriteLine("Book added successfully.");
+
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+      }
+
+    }
+
+    static void CheckOutBook (Library library)
+    {
+      string title = GetResponse("Enter title to check out: ");
+
+      try
+      {
+
+        library.CheckOutBook(title);
+        Console.WriteLine($"{title} has been checked out.");
+
+
+      }
+      catch (NullReferenceException e)
+      {
+        //book not found
+        Console.WriteLine(e.Message);
+      }
+      catch (Exception e)
+      {
+        // book is checked out
+        Console.WriteLine(e.Message);
+      }
+
+
+    }
+
+
+    static void ReturnBook(Library library)
+    {
+      string title = GetResponse("Enter title to return: ");
+
+      try
+      {
+
+        library.ReturnBook(title);
+        Console.WriteLine($"{title} has been returned.");
+
+
+      }
+      catch (NullReferenceException e)
+      {
+        //book not found
+        Console.WriteLine(e.Message);
+      }
+      catch (Exception e)
+      {
+        // book is not checked out
+        Console.WriteLine(e.Message);
+      }
+
+
+
     }
 }
